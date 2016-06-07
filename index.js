409c0d5e3efc87a5bb143d20ec3e9a488907ecd8
Marco Brizi <m.brizi@cineca.it>
2016-06-07 17:59:40 +0200

    connection moved to test

--- index.js
+++ index.js
@@ -1,66 +1,64 @@
-var mongodb = require('mongodb');
-var MongoClient = require('mongodb').MongoClient;
 var bcrypt = require('bcrypt');
 
-module.exports = {
+module.exports = function(opts) {
+    var db = opts.db;
+    return {
+        addUser: function(username, password, email, callback) {
+            // hashing password
+            var hash = hashPassword(password);
 
-  addUser : function(username, password, email, callback){
-    // hashing password
-    var hash = hashPassword(password);
+            //define user
+            var user = {
+                'username': username,
+                'password': hash,
+                'email': email,
+            }
 
-    //define user
-    var user = {
-      'username': username,
-      'password': hash,
-      'email': email,
+            // insert to db
+            var users = db.collection('users');
+            users.insert(user, function(err, result) {
+                console.log('user ' + username + ' added');
+                callback(err);
+            });
+        },
+
+        getUserInfo: function(username, callback) {
+
+            // fetch user
+            var users = db.collection('users');
+            users.findOne({
+                'username': username
+            }, {
+                '_id': 0,
+                'password': 0
+            }, function(err, user) {
+                console.log('user ' + username + ' found');
+                callback(err, user);
+            });
+        },
+
+        authenticate: function(username, password, callback) {
+            // fetch user
+            var users = db.collection('users');
+            users.findOne({
+                'username': username
+            }, {
+                '_id': 0
+            }, function(err, user) {
+                callback(err, verifyPassword(password, user.password));
+            });
+        }
     }
-
-    // insert to db
-    mongoConnect(function(err, db) {
-      var users = db.collection('users');
-      users.insert(user, function(err, result) {
-        db.close();
-        console.log("user " + username + " added");
-        callback(err);
-      });
-    });
-  },
-
-  getUserInfo : function(username, callback) {
-
-    // fetch user
-    mongoConnect(function(err, db) {
-      var users = db.collection('users');
-      users.findOne({'username':username},{'_id':0,'password':0}, function(err, user) {
-        db.close();
-        console.log("user " + username + " found");
-        callback(err, user);
-      });
-    });
-  },
-
-  authenticate : function(username, password, callback) {
-
-    // fetch user
-    mongoConnect(function(err, db) {
-      var users = db.collection('users');
-      users.findOne({'username':username},{'_id':0}, function(err, user) {
-        db.close();
-        callback(err, verifyPassword(password,user.password));
-      });
-    });
-  }
-
 }
 
 function mongoConnect(callback) {
-  MongoClient.connect('mongodb://localhost:27017/test', callback);
+    MongoClient.connect('mongodb://localhost:27017/test', callback);
 }
 
 function hashPassword(plainPassword) {
-  return bcrypt.hashSync(plainPassword, 10);
+    return bcrypt.hashSync(plainPassword, 10);
 }
 
 function verifyPassword(plainPassword, hash) {
-  return bcrypt.compareSync(plainPassword, hash);
+    return bcrypt.compareSync(plainPassword, hash);
 }
--- test/basetest.js
+++ test/basetest.js
@@ -1,35 +1,36 @@
 var tap = require('tap');
-var auth = require('../index');
+var build = require('../index');
 var clean = require('mongo-clean');
 
-var MongoClient = require('mongodb').MongoClient
-var url = "mongodb://localhost:27017/test"
+var url = 'mongodb://localhost:27017/test';
 
-MongoClient.connect(url, { w: 1 }, function (err, db) {
-  clean(db, function () {
-  db.close();
+clean(url, function(err, db) {
+    tap.tearDown(db.close.bind(db));
 
-    auth.addUser('marco','passwd','marco.brizi@gmail.com', (err) => {
-      tap.error(err);
-
-      auth.getUserInfo('marco', function(err,user){
-          tap.error(err);
-          tap.equal(user.email,'marco.brizi@gmail.com');
-          tap.equal(user.password,undefined);
-
-          auth.authenticate('marco','passwd',function(err, result){
-              tap.error(err);
-              tap.equal(result,true);
-              
-              tap.end();
-
-          });
-
-      });
+    var auth = build({
+        db
     });
 
-  });
-});
+    auth.addUser('marco', 'passwd', 'marco.brizi@gmail.com', (err) => {
+        tap.error(err);
+
+        auth.getUserInfo('marco', function(err, user) {
+            tap.error(err);
+            tap.equal(user.email, 'marco.brizi@gmail.com');
+            tap.equal(user.password, undefined);
+
+            auth.authenticate('marco', 'passwd', function(err, result) {
+                tap.error(err);
+                tap.equal(result, true);
+
+                tap.end();
+
+            });
+
+        });
+    });
+})
+
 
 
 
